77cca7dd2aa8a30c151303f264d4ab69d6d31d97
Kate <ktorochkova@gmail.com>
2022-12-05 21:08:54 +0300

    answer

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int s = 1100;
+        int p = 1001;
+
+        int bonus;
+        if (p >= 1001) {
+            bonus = p / 100;
+        } else {
+            bonus = 0;
+        }
+        int b = s + p + bonus;
+
+        System.out.println("Ваш счёт пополнен. Ваш баланс составляет " + b + " рублей");
+        System.out.println("Вам начислено " + bonus + " рублей");
     }
+
+
 }
